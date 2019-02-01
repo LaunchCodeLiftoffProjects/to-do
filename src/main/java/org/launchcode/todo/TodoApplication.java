2bfe6e8fce218d04a34ab2587a445ab0153f1772
Rajyalaxmi <gaddala.rajyalaxmi14@gmail.com>
2019-01-31 19:13:10 -0600

    created homecontroller

---
+++ src/main/java/org/launchcode/todo/TodoApplication.java
@@ -0,0 +1,16 @@
+package org.launchcode.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+import static org.springframework.boot.SpringApplication.*;
+
+@SpringBootApplication
+public class TodoApplication {
+	public static void main(String[] args) {
+		run(TodoApplication.class, args);
+	}
+
+	}
+
---
+++ src/test/java/org/launchcode/todo/TodoApplicationTests.java
@@ -0,0 +1,17 @@
+package org.launchcode.todo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TodoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
