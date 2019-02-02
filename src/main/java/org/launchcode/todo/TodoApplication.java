3ea713700491b30b3e79a27097475920bbadfd92
Latasha Howard <l_howard@outlook.com>
2019-02-01 22:04:51 -0600

    Added .gitignore file and dependencies

--- src/main/java/org/launchcode/todo/TodoApplication.java
+++ src/main/java/org/launchcode/todo/TodoApplication.java
@@ -3,13 +3,10 @@ package org.launchcode.todo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-import static org.springframework.boot.SpringApplication.*;
-
 @SpringBootApplication
 public class TodoApplication {
 	public static void main(String[] args) {
-		run(TodoApplication.class, args);
+		SpringApplication.run(TodoApplication.class, args);
 	}
 
 	}
