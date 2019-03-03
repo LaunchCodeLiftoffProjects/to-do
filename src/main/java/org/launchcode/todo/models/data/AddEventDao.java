1dc9ccd09f60859026502989b065b26ba3783fe1
shyamala <shyamala_theni@yahoo.com>
2019-03-03 14:39:48 -0600

    Displaying all events for current user

--- src/main/java/org/launchcode/todo/models/data/AddEventDao.java
+++ src/main/java/org/launchcode/todo/models/data/AddEventDao.java
@@ -3,13 +3,16 @@ package org.launchcode.todo.models.data;
 
 //import org.launchcode.todo.models.AddEvent;
 import org.launchcode.todo.models.Event;
+import org.launchcode.todo.models.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 @Transactional
 public interface AddEventDao extends CrudRepository <Event, Integer> {
 
-
+List<Event> findByUser(User user);
 }
